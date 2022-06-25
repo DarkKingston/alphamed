a95e789f8b412de2c15a6e9dfff5e60d792f98b1
Dorin <palii.dorin@yandex.ru>
2022-06-25 17:46:51 +0300

    added tabs about clinics

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,7 +1,7 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.slider-bg', {
     direction: 'horizontal',
     loop: true,
-    slidePerView: 1,
+    slidesPerView: 1,
     pagination: {
       el: '.swiper-pagination',
       type: 'bullets',
@@ -11,5 +11,25 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  });
+});
   
+const swiperStreet = new Swiper('.streets', {
+    direction: 'horizontal',
+    slidesPerView: 6,
+});
+
+
+let streets = document.querySelectorAll('.street_wrapper');
+let tab = document.querySelectorAll('.tab__info');
+for(let i = 0; i < streets.length; i++){
+    streets[i].addEventListener('click', function(){
+        for(let j = 0; j < streets.length; j++){
+            streets[j].classList.remove('street-active');
+        }
+        for(let k = 0; k < tab.length; k++){
+            tab[k].classList.remove('tab-active');
+        }
+        streets[i].classList.add('street-active');
+        tab[i].classList.add('tab-active');
+    });
+};
