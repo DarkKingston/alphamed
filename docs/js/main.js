793dba8711fbacf00ed3c9ec362b79dc6f660df2
Dorin <palii.dorin@yandex.ru>
2022-06-25 14:59:08 +0300

    adding slider

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,15 @@
-$(function () {
-
-})
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidePerView: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
